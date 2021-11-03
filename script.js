bf51050b1bb9df92eea736d677f6ea35fdb53e2a
afshakhanum <afshakhanstar@gmail.com>
2021-11-03 14:56:53 +0530

    first commit

--- script.js
+++ script.js
@@ -1,29 +1,74 @@
 
-// pecentage of total number
+// // pecentage of total number
 
-function result(a,b) {
- var percent=a/b*100;
- return percent;   
-}
-var c= result(40,150);
-console.log(c);
+// function result(a,b) {
+//  var percent=a/b*100;
+//  return percent;   
+// }
+// var c= result(40,150);
+// console.log(c);
 
-// even or odd number
+// // even or odd number
 
-function evrod(num) {
-   return num%2 ===0?'even':'odd' ;
-}
-var a=evrod(7);
-console.log(a);
+// function evrod(num) {
+//    return num%2 ===0?'even':'odd' ;
+// }
+// var a=evrod(7);
+// console.log(a);
 
 // reverse string
 
-function reverseString(str) {
-    var newString = "";
-    for (var i = str.length - 1; i >= 0; i--) {
-        newString += str[i];
-    }
-    return newString;
-}
- var a =reverseString('Javascript');
-console.log(a);
+// function revstr(str){
+//     var s ="";
+//     for (var i = str.length - 1; i >= 0; i--) {
+//         s += str[i];
+//     }
+//     return s;
+// }
+//  var a =revstr('anna');
+// console.log(a);
+
+// for palindrome
+
+// if(str===s){
+//     return 
+// }
+// else{
+//     console.log(NotPalindrome);
+// }
+
+// for factorial
+// function factorial(num){
+//     var n= 1;
+//     if(num==0 || num ==1){
+//         return "1";
+//     }
+//     else{
+//        for(var i=num;i >=3;i--){
+//         n *=i;   
+//        }
+//        return n;
+//     }
+// }
+// console.log(factorial(3));
+
+//Array
+
+// var color =['red','black','grey'];
+//   console.log(color);
+
+// color.push('blue');
+// console.log(color);
+
+// color.pop();
+// console.log(color);
+
+// color.unshift('yellow');
+// console.log(color);
+
+// color.shift();
+// console.log(color);
+
+var color =['red','black','grey','yellow','blue','orange'];
+color.sort();
+console.log(color);
